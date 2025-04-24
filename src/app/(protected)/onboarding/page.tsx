7ac16f1778365b2ba98947a87d6d40fec2366c4a
Macharia11 <stevieskaka3096@gmail.com>
2025-04-24 11:55:30 +0300

    mymessage

--- src/app/(protected)/onboarding/page.tsx
+++ src/app/(protected)/onboarding/page.tsx
@@ -1,18 +1,51 @@
-import { onBoardUser } from '@/actions/user'
+// import { onBoardUser } from '@/actions/user'
+// import { redirect } from 'next/navigation'
+// import {client} from '@/lib/prisma'
+// import {onCurrentUser} from '@/actions/user'
+// import React from 'react'
+
+// type Props = {}
+
+// const Page = async (props: Props) => {
+//   const user = await onCurrentUser()
+//   if (!user) {
+//     return redirect('/sign-in')
+//   }
+//   if (user && client.business) {
+//     return redirect('/dashboard')
+//   }
+
+//   if (user && client.influencer) {
+//     return redirect('/influencer')
+//   }
+
+//   return redirect('/onboarding/new')
+// }
+
+// export default Page
+
 import { redirect } from 'next/navigation'
-import {client} from '@/lib/prisma'
-import {onCurrentUser} from '@/actions/user'
-import React from 'react'
+import { client } from '@/lib/prisma'
+import { onCurrentUser } from '@/actions/user'
 
-type Props = {}
-
-const Page = async (props: Props) => {
+const Page = async () => {
   const user = await onCurrentUser()
+
   if (!user) {
     return redirect('/sign-in')
   }
-  if (user && client.business) {
-    return redirect('/sign-in')
+
+  const [userBusiness, userInfluencer] = await Promise.all([
+    client.business.findFirst({ where: { userId: user.id } }),
+    client.influencer.findFirst({ where: { userId: user.id } }),
+  ])
+
+  if (userBusiness) {
+    return redirect('/dashboard')
+  }
+
+  if (userInfluencer) {
+    return redirect('/influencer')
   }
 
   return redirect('/onboarding/new')
