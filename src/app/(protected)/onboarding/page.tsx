cb88a65d74c69aa171badd02d4a3b3eae38391c5
Macharia11 <stevieskaka3096@gmail.com>
2025-04-24 18:40:21 +0300

    mymessage

--- src/app/(protected)/onboarding/page.tsx
+++ src/app/(protected)/onboarding/page.tsx
@@ -24,30 +24,62 @@
 
 // export default Page
 
+// import { redirect } from 'next/navigation'
+// import { client } from '@/lib/prisma'
+// import { onCurrentUser } from '@/actions/user'
+// import { onUserInfor } from '@/actions/user'
+
+// const Page = async () => {
+//   const user = await onCurrentUser()
+//   const thisuser =  await onUserInfor()
+//   const userid = thisuser.data?.id
+
+//   if (!user) {
+//     return redirect('/sign-in')
+//   }
+
+//   const [userBusiness, userInfluencer] = await Promise.all([
+//     client.business.findFirst({ where: { userId: userid } }),
+//     client.influencer.findFirst({ where: { userId: userid } }),
+//   ])
+
+//   if (userBusiness) {
+//     return redirect('/dashboard')
+//   }
+
+//   if (userInfluencer) {
+//     return redirect('/influencer')
+//   }
+
+//   return redirect('/onboarding/new')
+// }
+
+// export default Page
+
 import { redirect } from 'next/navigation'
 import { client } from '@/lib/prisma'
 import { onCurrentUser } from '@/actions/user'
-import { onUserInfor } from '@/actions/user'
 
 const Page = async () => {
   const user = await onCurrentUser()
-  const thisuser =  await onUserInfor()
-  const userid = thisuser.data?.id
 
   if (!user) {
     return redirect('/sign-in')
   }
 
-  const [userBusiness, userInfluencer] = await Promise.all([
-    client.business.findFirst({ where: { userId: userid } }),
-    client.influencer.findFirst({ where: { userId: userid } }),
-  ])
+  const userInfo = await client.user.findUnique({
+    where: { clerkId: user.id },
+    select: {
+      isABusiness: true,
+      isInfluencer: true,
+    },
+  })
 
-  if (userBusiness) {
+  if (userInfo?.isABusiness) {
     return redirect('/dashboard')
   }
 
-  if (userInfluencer) {
+  if (userInfo?.isInfluencer) {
     return redirect('/influencer')
   }
 
