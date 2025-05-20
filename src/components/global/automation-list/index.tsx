0d585b61a3a8353546e5d3b34546ec54f454800a
Macharia11 <stevieskaka3096@gmail.com>
2025-05-20 10:10:44 +0300

    Configure

--- src/components/global/automation-list/index.tsx
+++ src/components/global/automation-list/index.tsx
@@ -359,29 +359,6 @@ const AutomationList = ({ id }: Props) => {
             </div>
           </div>
         </div>
-
-        // <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
-        //   <div className="bg-primary p-6 rounded-lg text-white shadow-lg w-80">
-        //     <h2 className="text-lg font-semibold mb-4">Confirm Delete</h2>
-        //     <p className="text-sm mb-6">This action cannot be undone!</p>
-        //     <div className="flex justify-end gap-3">
-        //       <Button className="bg-gray-300 text-black hover:bg-gray-400" onClick={() => setShowConfirmModal(false)}>
-        //         Cancel
-        //       </Button>
-        //       <Button
-        //         className="bg-red-600 hover:bg-red-700"
-        //         onClick={() => {
-        //           if (selectedAutomationId) {
-        //             handleDelete(selectedAutomationId)
-        //           }
-        //           setShowConfirmModal(false)
-        //         }}
-        //       >
-        //         Delete
-        //       </Button>
-        //     </div>
-        //   </div>
-        // </div>
       )}
     </div>
   )
